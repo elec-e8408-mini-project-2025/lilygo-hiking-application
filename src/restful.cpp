3629f1f35f177af127eda4eebfbb5fac37c76797
JaajoJ <jage.lunden@gmail.com>
2025-02-04 18:34:39 +0200

    restful packet address and type parse

--- src/restful.cpp
+++ src/restful.cpp
@@ -1,1 +1,120 @@
 #include "restful.h"
+
+restfulAddressView mainView;
+
+restfulAddressView tripsView;
+
+int initRestful(tripData *trips, int size)
+{
+    mainView.address = RESTFUL_MAIN_VIEW;
+    char *fixedAddresses[] = FIXED_ADDRESSES;
+    mainView.otherAddresses = fixedAddresses;
+    mainView.dataCount = FIXED_ADDRESSES_COUNT;
+
+    char tripAddressesTmp[MAX_CACHED_TRIPS][sizeof(RESTFUL_INDIVIDUAL_TRIP_VIEW)];
+    char sampleTripAddress[] = RESTFUL_INDIVIDUAL_TRIP_VIEW; 
+    for (int i = 0; i < MAX_CACHED_TRIPS; ++i)
+    {
+        for (int i2 = 0; i2 < sizeof(RESTFUL_INDIVIDUAL_TRIP_VIEW); ++i2)
+        {
+            tripAddressesTmp[i][i2] = sampleTripAddress[i2];
+        }
+        tripAddressesTmp[i][sizeof(RESTFUL_INDIVIDUAL_TRIP_VIEW) - 2] = (char) i + 48; 
+    }
+    
+    char *tripAddresses[MAX_CACHED_TRIPS];
+    char **tripAddressesPointer = tripAddresses;
+    for (int i = 0; i < MAX_CACHED_TRIPS; ++i)
+    {
+        tripAddresses[i] = tripAddressesTmp[i];
+    }
+    tripsView.otherAddresses = tripAddressesPointer;
+    tripsView.address = RESTFUL_TRIPS_VIEW;
+    tripsView.dataCount = MAX_CACHED_TRIPS;
+    return 0;
+}
+
+
+httpType restfulParseType(const char* data, const int *dataLen)
+{
+    httpType ret;
+    if (*dataLen < 1)
+    {
+        ret = RESTFUL_ERROR;
+        return ret;
+    }
+    switch (data[0])
+    {
+        case 'G':
+            ret = RESTFUL_GET;
+            break;
+        case 'P':
+            ret = RESTFUL_POST;
+            break;
+        default:
+            ret = RESTFUL_ERROR;
+            break;
+    } 
+    return ret;
+} 
+
+int restfulParseAddress(restfulPacket * packet, char * data, const int * dataLen)
+{
+    char address[21] = "";
+    int addressOffset = 0;
+    bool addresspart; 
+
+    switch (packet->type)
+    {
+        case RESTFUL_GET:
+            addressOffset = 4;
+            break;
+        case RESTFUL_POST:
+            addressOffset = 5;
+            break;
+        default:
+            return -1;
+            break;
+    };
+    
+    packet->addressData = data + addressOffset;
+    packet->addressLen = *dataLen - addressOffset;
+
+    return 0;
+}
+
+int parseRestfulPacket(restfulPacket * packet, char * data, const int * dataLen)
+{
+    packet->type = restfulParseType(data,dataLen);
+    restfulParseAddress(packet, data, dataLen);
+    return 0;
+}
+
+
+restfulPacket restfulHandlePacket(char *data, const int *dataLen)
+{
+    restfulPacket ret = {
+        0,
+        0,
+        "",
+        0,
+        0,
+        0,
+        RESTFUL_ERROR
+    };
+    
+    parseRestfulPacket(&ret, data, dataLen);
+
+    switch (ret.type)
+    {
+        case RESTFUL_GET:
+            break;
+        case RESTFUL_POST:
+            break;
+        default:
+            break;
+    }
+
+    return ret;
+
+}
--- src/restful.h
+++ src/restful.h
@@ -1,19 +1,68 @@
 #ifndef RESTFUL_H
 #define RESTFUL_H
+#include "config.h"
+#include "data.h"
 
-#define MAXDATALENGTH 64
+#define RESTFUL_MAIN_VIEW "/"
+#define RESTFUL_TRIPS_VIEW "/trips"
+#define RESTFUL_INDIVIDUAL_TRIP_VIEW "/trips/0"
 
-enum dataFrameType {
-    GET,
-    POST
+#ifndef ESP32_WROOM_32
+#define RESTFUL_TRIPS_VIEW_GEOLOCATION "/trips/x/geolocation"
+#endif
+
+#define FIXED_ADDRESSES { \
+                    RESTFUL_MAIN_VIEW, \
+                    RESTFUL_TRIPS_VIEW \
+                    }
+#define FIXED_ADDRESSES_COUNT 2
+
+
+typedef struct {
+    char * address;
+    char ** otherAddresses;
+    int dataCount;
+} restfulAddressView;
+
+enum restfulTripStatus{
+    SYNCED,
+    NOT_SYNCED,
+    ERROR
 };
 
-typedef struct{
-    char data[MAXDATALENGTH];
-    signed int dataLength;
-    dataFrameType type;
-} restfulDataFrame;
+typedef struct {
+    tripData * trips;
+    restfulTripStatus status;
+} restfulTripData;
 
+#ifndef ESP32_WROOM_32
+// PLACE HOLDER NOT YET IMPLEMENTED
+typedef struct {
+    geoData * data;
+    restfulTripStatus status;
+} restfulGeolocation;
+#endif
+
+typedef enum {
+    RESTFUL_GET,
+    RESTFUL_POST,
+    RESTFUL_ERROR
+} httpType;
+
+typedef struct {
+    char *addressData;
+    int addressLen;
+    char postData[32];
+    int postDataLen;
+    char *response;
+    int responseLen;
+    httpType type;
+} restfulPacket;
+
+
+int initRestful(tripData *trips, int size);
+
+restfulPacket restfulHandlePacket(char *data, const int *dataLen);
 
 
 
